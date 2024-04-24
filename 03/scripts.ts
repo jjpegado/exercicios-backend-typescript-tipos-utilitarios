57d0d193b91c2e8d5321448b30a73b20e3784e65
jjpegado <julio.tenorio_11@hotmail.com>
2024-04-24 09:33:18 -0300

    exercicio finalização

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,26 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type Conexao = {
+    driver:string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+
+const conectar = conexao ({
+    username: 'julio', 
+    password: '99882644', 
+    host: 'localhost', 
+    port: '5432', 
+    dbname: 'primeira'  
+})
+
+conectar.driver = 'mySQL' //Readonly não pode fazer mudanças so leitura
